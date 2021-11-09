bba4797c1b2ecd03ac2bd6102d541386d708d2a4
patricia milane <patriciamilane@hotmail.com>
2021-11-09 12:15:57 -0300

    first commit

---
+++ js/owl/setup.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+//https://owlcarousel2.github.io/OwlCarousel2/demos/basic.html
